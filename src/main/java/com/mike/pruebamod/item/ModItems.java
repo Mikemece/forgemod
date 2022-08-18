35b82f12ce8ba35632348cb4fef513601087ea23
Mikemece <mcmiguel313@gmail.com>
2022-08-18 18:06:17 +0200

    pito added

---
+++ src/main/java/com/mike/pruebamod/item/ModItems.java
@@ -0,0 +1,23 @@
+package com.mike.pruebamod.item;
+
+import com.mike.pruebamod.PruebaMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    //Esto es como una lista de items
+    public static final DeferredRegister<Item> ITEMS=
+            DeferredRegister.create(ForgeRegistries.ITEMS, PruebaMod.MOD_ID);
+
+//primer objeto añadido
+    public static final RegistryObject<Item> PITO = ITEMS.register("pito",
+            ()-> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
